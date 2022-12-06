fe1c6fb086bc37633184034031903dffbb3819ec
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-06 15:22:32 -0500

    added base function and notes on prompt

---
+++ binary_search_pseudocode.js
@@ -0,0 +1,14 @@
+// only numbers
+// accepts a sorted array and a value
+// make a left and right pointer
+// left starts at 0
+// loop only while the left comes before the right pointer
+// check if the element is equal to the middle 
+// if that value matches, return the index
+// if the value is too small, move the left pointer up
+// if the value is too large, move the right pointer down
+// if you don't find the value, return -1
+
+function binarySearchPseudocode(){
+
+}
