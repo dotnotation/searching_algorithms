7438981dda3f7b23da0a90d659124b32673381a3
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-06 16:40:56 -0500

    adding prompt and base function

---
+++ string_search.js
@@ -0,0 +1,9 @@
+// takes in a long string, and a short string
+// loop over the longer string
+// nested loop of the shorter string
+// return the count of times the shorter string appears
+
+function stringSearch(long, short){
+    
+
+}
