92d6e78e2109dbdde951295c286d69051bff518a
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-06 14:18:20 -0500

    adding notes and base function

---
+++ linear_search_pseudocode.js
@@ -0,0 +1,12 @@
+// function that accepts an array and a value
+// loop through the array and check if the current array element is equal to the value 
+// if found, return the index
+// if not found, return -1 
+// ex. linearSearch([10, 15, 20, 25, 30], 15) // 1
+// linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4) // 5
+// linearSearch([100], 100) // 0
+// linearSearch([1,2,3,4,5], 6) // -1
+
+function linearSearchPseudocode(arr, val){
+
+}
