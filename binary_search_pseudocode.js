1da1799ecc04c3f5b2d69f69d6af90ab8dec4c1c
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-06 15:30:38 -0500

    added my solution

--- binary_search_pseudocode.js
+++ binary_search_pseudocode.js
@@ -9,6 +9,20 @@
 // if the value is too large, move the right pointer down
 // if you don't find the value, return -1
 
-function binarySearchPseudocode(){
-
+function binarySearchPseudocode(arr, val){
+    let min = 0
+    let max = arr.length - 1
+    
+    while (min <= max){
+        let middle = Math.floor((min + max) / 2)
+        let current = arr[middle]
+        if (current < val) {
+            min = middle + 1
+        } else if (current > val){
+            max = middle - 1
+        } else {
+            return middle
+        }
+    }
+    return -1
 }
