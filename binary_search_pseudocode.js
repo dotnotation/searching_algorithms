1918731c6ebb8b990a5ce2c7098822fbf78df345
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-06 16:01:45 -0500

    added Colt Steele's solutions

--- binary_search_pseudocode.js
+++ binary_search_pseudocode.js
@@ -26,3 +26,38 @@ function binarySearchPseudocode(arr, val){
     }
     return -1
 }
+
+// Colt Steele's Solution
+
+function binarySearchPseudocode(arr, elem){
+    var start = 0
+    var end = arr.length - 1
+    var middle = Math.floor((start + end) / 2)
+
+    while(arr[middle] !== elem && start <= end){
+        if (elem < arr[middle]){
+            end = middle - 1
+        } else {
+            start = middle + 1
+        }
+        middle = Math.floor((start + end) / 2)
+    }
+    if (arr[middle] === elem){
+        return middle
+    }
+    return -1
+}
+
+// cleaner version
+function binarySearchPseudocode(arr, elem){
+    var start = 0
+    var end = arr.length - 1
+    var middle = Math.floor((start + end) / 2)
+
+    while(arr[middle] !== elem && start <= end){
+        if (elem < arr[middle]) end = middle - 1
+        else start = middle + 1
+        middle = Math.floor((start + end) / 2)
+    }
+    return arr[middle] === elem ? middle : -1
+}
