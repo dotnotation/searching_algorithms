7aa3420749a4a57b383afc4cbef9d23096c0f77d
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-09 22:10:37 -0500

    added notes

---
+++ bubble_sort.js
@@ -0,0 +1,11 @@
+// start looping from with a variable called i the end of the array towards the beginning
+// start an inner loop with a variable called j from the beginning until i - 1
+// if arr[j] is greater than arr[j+1], swap those two values
+// return the sorted array
+// take the variable i and compare it to the adjacent element
+// if it is larger, more it towards the back 
+// nested loop that is depending on the first loop 
+
+function bubbleSort(){
+
+}
