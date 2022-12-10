3ee7b1842b15ace83efa81f14769fca1443e3034
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-09 21:56:07 -0500

    added code along

---
+++ swap.js
@@ -0,0 +1,13 @@
+// bubble sort
+
+// ES5
+function swap(arr, idx1, idx2){
+    var temp = arr[idx1]
+    arr[idx1] = arr[idx2]
+    arr[idx2] = temp
+}
+
+// ES2015
+function swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
+}
