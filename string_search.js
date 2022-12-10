de9a37dcd94fae085d7f979292024fa06fdf63b2
Dorthy Thielsen <dorthythielsen@gmail.com>
2022-12-09 21:17:41 -0500

    added notes

--- string_search.js
+++ string_search.js
@@ -4,6 +4,35 @@
 // return the count of times the shorter string appears
 
 function stringSearch(long, short){
-    
+    let count = 0
+    for (var i = 0; i < long.length; i ++){
+        for (var j = 0; j < short.length; j ++){
+            // console.log(long[i], short[j])
+            // console.log(short[j], long[i+j])
+            if (short[j] !== long[i + j]){
+                // anytime there isn't a match, we want to break from this loop
+                // this will rest our j loop and continue the i loop
+                // console.log("BREAK!")
+                break
+            }
+            if (j === short.length - 1){
+                console.log("found one!")
+                count++
+            }
+        }
+    }
+    return count
 
+}
+
+// Colt's cleaner version
+function stringSearch(long, short){
+    var count = 0
+    for (var i = 0; i < long.length; i++){
+        for (var j = 0; j < short.length; j++){
+            if (short[j] !== long[i+j]) break
+            if (j === short.length - 1) count ++
+        }
+    }
+    return count
 }
