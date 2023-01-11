0e28915e2b4bbf56835f531f8ba99479594c10bb
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-11 15:16:10 -0500

    added base function and notes

---
+++ insertion_sort.js
@@ -0,0 +1,10 @@
+// start by picking the second element in the array
+// compare the second element to the one before it and swap if necessary
+// continue to the next element
+// if it is not in the correct order, iterate through the sorted portion (the left side) 
+// place the element in the correct place in the sorted portion
+// return sorted array
+
+function insertionSort(arr){
+
+}
