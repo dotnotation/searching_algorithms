97b281db49d56e0779d0ebfbc5579a868cf8361e
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-11 15:39:00 -0500

    added loop skeleton and more notes

--- insertion_sort.js
+++ insertion_sort.js
@@ -3,8 +3,21 @@
 // continue to the next element
 // if it is not in the correct order, iterate through the sorted portion (the left side) 
 // place the element in the correct place in the sorted portion
+// to sort, you will be comparing the element at the end of the sorted portion to the current element 
+// swap them until you have the element in the correct spot
 // return sorted array
+// [1, 2, 9, 76, 0] 
+    // store current as 0
+    // 0 is less than 76 so replace 0 with 76 [1, 2, 9, 76, 76]
+    // 0 is less than 9 so [1, 2, 9, 9, 76]
+    // [1, 2, 2, 9, 76] etc. until the arr is [0, 1, 2, 9, 76]
 
 function insertionSort(arr){
-
+    for (let i = 1; i < arr.length; i++){
+        let current = arr[i]
+        for (let j = i - 1; j >= 0; j--){
+            arr[j] > current
+        }
+    }
+    return arr
 }
