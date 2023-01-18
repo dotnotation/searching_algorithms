6057973f72b3503e9dec4061bf825e1573d2943e
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-18 17:26:24 -0500

    added solution to merge sort

--- merge_sort.js
+++ merge_sort.js
@@ -7,6 +7,31 @@
 // 		- if the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
 // 		- once one array is exhausted, push in all remaining values from the other array
 
-function mergeSort(arr){
-    
+function mergeSort(arr1, arr2){
+    let results = []
+
+    let i = 0
+    let j = 0
+
+    while (i < arr1.length && j < arr2.length){
+        if (arr2[j] > arr1[i]){
+            results.push(arr1[i])
+            i++
+        } else {
+            results.push(arr2[j])
+            j++
+        }
+    }
+
+    while (i < arr1.length){
+        results.push(arr1[i])
+        i++
+    }
+
+    while (j < arr2.length){
+        results.push(arr2[j])
+        j++
+    }
+
+    return results
 }
