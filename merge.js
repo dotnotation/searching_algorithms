1d79911971f8d378c19a8e08dfc8d433b029f3fc
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-18 17:27:49 -0500

    renamed merge sort to merge

--- merge.js
+++ merge.js
@@ -7,7 +7,7 @@
 // 		- if the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
 // 		- once one array is exhausted, push in all remaining values from the other array
 
-function mergeSort(arr1, arr2){
+function merge(arr1, arr2){
     let results = []
 
     let i = 0
