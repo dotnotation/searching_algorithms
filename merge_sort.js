b5fc8a284ebd1a8a7aea8b3c48562f156d663722
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-18 17:11:38 -0500

    added notes and base function for merge sort

---
+++ merge_sort.js
@@ -0,0 +1,12 @@
+// - Create an empty array, take a look at the smallest values in each input array
+// 	- have two counters with while loops
+// 		- both i and j start at 0
+// 		- have a results array
+// 		- while there are still values we haven't looked at
+// 			- while i and j haven't hit the end of their respective arrays
+// 		- if the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
+// 		- once one array is exhausted, push in all remaining values from the other array
+
+function mergeSort(arr){
+    
+}
