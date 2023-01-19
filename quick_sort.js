4ffb2d7f01e4f57c04d0543e9dd0caa8e247cc81
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-19 17:56:01 -0500

    added notes and base function for pivot the helper function for quick sort

---
+++ quick_sort.js
@@ -0,0 +1,13 @@
+// - accept three arguments (array, start index, and end index)
+// 	- these can default to 0 and to the array length - 1
+// - grab the pivot from the start of the array
+// - store the current pivot index in a variable
+// 	- this will keep track of where the pivot should end up
+// - loop through the array from the start until the end
+// 	- if the pivot is greater than the current element, increment the pivot index variable and then swap the current element with the element at the pivot index
+// - swap the starting element(the pivot) with the pivot index
+// - return the pivot index
+
+function pivot(arr, start, end){
+
+}
