78c508b2cf192c00b13e4b9c77648fe15f96a131
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-25 16:13:46 -0500

    add mostDigits function and more notes

--- radix_sort.js
+++ radix_sort.js
@@ -13,10 +13,27 @@ function getDigit(num, place){
 
 // second helper
 // figure out how many digits are in a number
+// ex. digitCount(314) = 3
+// could do a string conversion or use math
 
 function digitCount(num){
-    
+    if (num === 0) return 1
+    // 0 would return -Infinity so need to account for that 
+    return Math.floor(Math.log10(Math.abs(num))) + 1
+    // log10 asks 10 to what power gets us num
+    // using abs so we can use this even with negative numbers
+    // using floor as it won't usually be an integer
 }
 
 // third helper
 // uses digitCount to figure out the largest number
+// given an array of numbers, returns the number of digits in the largest numbers in the list
+// ex. mostDigits([1234, 56, 7]) = 4
+
+function mostDigits(nums){
+    let maxDigits = 0
+    for (let i = 0; i < nums.length; i++){
+        maxDigits = Math.max(maxDigits, digitCount(nums[i]))
+    }
+    return maxDigits
+}
