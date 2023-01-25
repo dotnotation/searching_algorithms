76d75f42647e6db71481ad6a4cf3823a765a0375
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-25 15:39:34 -0500

    added notes and base function

---
+++ radix_sort.js
@@ -0,0 +1,11 @@
+// first helper for radix sort
+// getDigit(num, place)
+// returns the digit in num at the given place value
+// ex. getDigit(12345, 0) = 5
+// going the opposite direction as we want to follow the logic of a number
+// could convert into string and convert back however index would still be wrong
+// base ten numbers only and divide each number by ten to get the index
+
+function getDigit(num, place){
+    
+}
