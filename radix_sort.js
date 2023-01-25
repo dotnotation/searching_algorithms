23b3ce1dfa21c48231fd6e009f628523d694d465
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-01-25 15:52:48 -0500

    finished getDigit and more notes

--- radix_sort.js
+++ radix_sort.js
@@ -4,8 +4,8 @@
 // ex. getDigit(12345, 0) = 5
 // going the opposite direction as we want to follow the logic of a number
 // could convert into string and convert back however index would still be wrong
-// base ten numbers only and divide each number by ten to get the index
+// base ten numbers only so divide each number by ten to get the correct index
 
 function getDigit(num, place){
-    
+    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10
 }
